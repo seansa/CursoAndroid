840e1530232cf5286abc49c35120200b4fe02152
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-09-18 23:55:25 -0300

    [Declaracion y asignacion SegundaAvtivity]

--- app/src/main/java/com/hssait/cursoandroid/SegundaActivity.java
+++ app/src/main/java/com/hssait/cursoandroid/SegundaActivity.java
@@ -2,12 +2,21 @@ package com.hssait.cursoandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SegundaActivity extends Activity {
+
+    //Declaracion
+    Button btnSpinner, btnListView, btnMas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        //Asignacion
+        btnSpinner = (Button)findViewById(R.id.btnSpinner);
+        btnListView = (Button)findViewById(R.id.btnListView);
+        btnMas = (Button)findViewById(R.id.btnMasElementos);
     }
 }
