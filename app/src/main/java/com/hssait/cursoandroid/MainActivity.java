17940551874a282de7165ecf31c4975d857556d0
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-09-18 23:10:24 -0300

    [Declaracion y Asignacion] MainActivity Se declara y asigna los elementos de la interfaz grafica del MainActivity

--- app/src/main/java/com/hssait/cursoandroid/MainActivity.java
+++ app/src/main/java/com/hssait/cursoandroid/MainActivity.java
@@ -2,12 +2,23 @@ package com.hssait.cursoandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    //Declaracion
+    private Button btnAceptar;
+    private EditText txtUser, txtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Asignacion/Inicializacion
+        btnAceptar = (Button)findViewById(R.id.btnAceptarMain);
+        txtUser    = (EditText)findViewById(R.id.txtUsuario);
+        txtPass    = (EditText)findViewById(R.id.txtContrasenia);
     }
 }
