08ead0eede4ab8436d16c4321aa49cb21b5c3f8d
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-09-19 00:08:26 -0300

    [Se borra codigo innecesario en SegundaActivity]

--- app/src/main/java/com/hssait/cursoandroid/SegundaActivity.java
+++ app/src/main/java/com/hssait/cursoandroid/SegundaActivity.java
@@ -8,18 +8,10 @@ import android.widget.Button;
 
 public class SegundaActivity extends Activity {
 
-    //Declaracion
-    Button btnSpinner, btnListView, btnMas;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
-
-        //Asignacion
-        btnSpinner = (Button)findViewById(R.id.btnSpinner);
-        btnListView = (Button)findViewById(R.id.btnListView);
-        btnMas = (Button)findViewById(R.id.btnMasElementos);
     }
 
     public void LanzarSpinnerActivity(View view){
