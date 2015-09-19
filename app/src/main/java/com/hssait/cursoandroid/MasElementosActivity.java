e285605986f753d9a9839c68870a48b3c0abc981
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-09-18 23:59:25 -0300

    [Se crean nuevas actividades] *ListViewActivity, SpinnerActivity y MasElementosActivity

---
+++ app/src/main/java/com/hssait/cursoandroid/MasElementosActivity.java
@@ -0,0 +1,13 @@
+package com.hssait.cursoandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MasElementosActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mas_elementos);
+    }
+}
