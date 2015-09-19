beef459fd28862beea02ded173e97c3515f10d4b
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-09-19 00:23:25 -0300

    [Se inicializa la lista en ListViewActivity]

--- app/src/main/java/com/hssait/cursoandroid/ListViewActivity.java
+++ app/src/main/java/com/hssait/cursoandroid/ListViewActivity.java
@@ -2,12 +2,36 @@ package com.hssait.cursoandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListViewActivity extends Activity {
+
+    private ListView lista;
+    private Button btnAgrega;
+    private EditText txtItem;
+    private ArrayList<String> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
+        btnAgrega = (Button)findViewById(R.id.btnAgregarItemListView);
+        txtItem   = (EditText)findViewById(R.id.txtItemListView);
+        lista     = (ListView)findViewById(R.id.listaItems);
+        items = new ArrayList<>();
+
+        InicializarLista();
+    }
+
+    private void InicializarLista() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, items);
+        lista.setAdapter(adapter);
     }
 }
