88ca74f16e6a94f9973ac6fca33d0c24363c00de
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-09-19 00:57:55 -0300

    [Spinners creados e inicializados]

--- app/src/main/java/com/hssait/cursoandroid/SpinnerActivity.java
+++ app/src/main/java/com/hssait/cursoandroid/SpinnerActivity.java
@@ -2,12 +2,51 @@ package com.hssait.cursoandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class SpinnerActivity extends Activity {
+
+    private Spinner spinnerOpciones, spinnerValores;
+    private ArrayList<String> primero, segundo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spinner);
+
+        spinnerOpciones = (Spinner)findViewById(R.id.spinnerOpciones);
+        spinnerValores = (Spinner)findViewById(R.id.spinnerValores);
+        primero = new ArrayList<>();
+        segundo = new ArrayList<>();
+
+        CargarListas();
+        InicializarSpinners();
     }
+
+    private void InicializarSpinners() {
+
+        ArrayAdapter<String> adapterOpciones = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, primero);
+
+        ArrayAdapter<String> adapterValores = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, segundo);
+
+        spinnerOpciones.setAdapter(adapterOpciones);
+        spinnerValores.setAdapter(adapterValores);
+
+    }
+
+    private void CargarListas() {
+        primero.add("Opcion 1");
+        primero.add("Opcion 2");
+        primero.add("Opcion 3");
+
+        segundo.add("Valor 1");
+        segundo.add("Valor 2");
+        segundo.add("Valor 3");
+    }
+
 }
