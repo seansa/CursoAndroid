5984093a46f065526ec8816b83c008e20824c2b4
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-10-01 23:45:06 -0300

    [Modelo persona agregado]

---
+++ app/src/main/java/com/hssait/cursoandroid/modelo/Persona.java
@@ -0,0 +1,16 @@
+package com.hssait.cursoandroid.modelo;
+
+import com.activeandroid.Model;
+
+/**
+ * Created by seansa on 1/10/15.
+ */
+public class Persona extends Model {
+
+    public String nombre;
+    public String apellido;
+    public int    edad;
+
+    public Persona() {
+    }
+}
