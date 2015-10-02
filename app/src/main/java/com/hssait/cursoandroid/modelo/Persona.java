c8ee1fab5adf2613f48e9e1983fe1ea79832bcdc
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-10-01 23:46:24 -0300

    [Modelo persona activeandroid]

--- app/src/main/java/com/hssait/cursoandroid/modelo/Persona.java
+++ app/src/main/java/com/hssait/cursoandroid/modelo/Persona.java
@@ -1,14 +1,20 @@
 package com.hssait.cursoandroid.modelo;
 
 import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
 
 /**
  * Created by seansa on 1/10/15.
  */
+@Table(name = "Personas")
 public class Persona extends Model {
 
+    @Column(name = "nombre")
     public String nombre;
+    @Column(name = "apellido")
     public String apellido;
+    @Column(name = "edad")
     public int    edad;
 
     public Persona() {
