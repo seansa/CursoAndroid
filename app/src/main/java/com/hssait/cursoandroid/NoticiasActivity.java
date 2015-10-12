358e189eb7155a401c95d69beb26819a332202c0
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-10-12 16:14:59 -0300

    [jsonCallback]

--- app/src/main/java/com/hssait/cursoandroid/NoticiasActivity.java
+++ app/src/main/java/com/hssait/cursoandroid/NoticiasActivity.java
@@ -2,8 +2,13 @@ package com.hssait.cursoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.androidquery.AQuery;
+import com.androidquery.callback.AjaxStatus;
+
+import org.json.JSONObject;
 
 public class NoticiasActivity extends AppCompatActivity {
 
@@ -13,5 +18,24 @@ public class NoticiasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticias);
         aQuery = new AQuery(this);
+
+        aQuery.id(R.id.btnNoticias).clicked(this,"TraerNoticias");
+    }
+
+    public void TraerNoticias(View view){
+        String url = "http://www.google.com/uds/GnewsSearch?q="+
+                aQuery.id(R.id.txtNoticias).getText().toString()+"&v=1.0";
+        aQuery.ajax(url, JSONObject.class, this, "jsonCallback");
+    }
+
+    public void jsonCallback(String url, JSONObject json, AjaxStatus status){
+
+        if(json != null){
+            
+        }else{
+            Toast.makeText(NoticiasActivity.this,
+                    "Error al realizar peticion GET. "
+                            + status.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
