9491c225ea787e3080b2c0fa0c16b461eed7e11d
Sebastian Salvatore <sebastiansalvatore@gmail.com>
2015-10-12 16:06:08 -0300

    [AQuery en ActivityNoticias]

--- app/src/main/java/com/hssait/cursoandroid/NoticiasActivity.java
+++ app/src/main/java/com/hssait/cursoandroid/NoticiasActivity.java
@@ -3,11 +3,15 @@ package com.hssait.cursoandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.androidquery.AQuery;
+
 public class NoticiasActivity extends AppCompatActivity {
 
+    AQuery aQuery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticias);
+        aQuery = new AQuery(this);
     }
 }
